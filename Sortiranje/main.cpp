ad7e0eb9a9b365509ea32d4cef8b52f58a9ca6a4
acenic <aca.cen@gmail.com>
2019-04-03 21:51:40 +0200

    Promena main-a

--- Sortiranje/main.cpp
+++ Sortiranje/main.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 void main()
 {
-	int A[] = { 1, 8, 5, 3, 2 };
+	//int A[] = { 1, 8, 5, 3, 2 };
 	Niz niz(5);
-	niz.mergesort(A, 5);
-	//niz.print();
-	for (int i = 0; i < 5; i++)
-		cout << A[i] << " ";
+	niz.SelectonSort();
+	niz.print();
+	//for (int i = 0; i < 5; i++)
+		//cout << A[i] << " ";
 }
